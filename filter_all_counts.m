d557cb50238f04e8138d8ef7339a413b97ad362f
Robert Sare <rmsare@stanford.edu>
2017-02-15 11:35:23 -0800

    Change default column index value

--- filter_all_counts.m
+++ filter_all_counts.m
@@ -2,12 +2,12 @@ function filter_all_counts(directory)
     filelist = dir(directory);
     
     for(i=3:numel(filelist))
-        infilename = filelist(i).name
+        infilename = filelist(i).name;
         if(strcmp(infilename(end-3:end), 'FIN2'))
             infilename = strcat(directory, infilename);
             nonblankidx = 143:290;
             [data, k] = read_counts(infilename, nonblankidx);
-            not_outliers = outlier_test(data, nonblankidx, 3, 5, k);
+            not_outliers = outlier_test(data, nonblankidx, 3, 0, k);
             save_counts(infilename, data, not_outliers, nonblankidx);
         end
     end
