71b37d11b2a4bf0d7108e98cd74e5b0c71cd4f47
Robert Sare <rmsare@stanford.edu>
2017-02-15 11:25:47 -0800

    Take directory as input path

--- filter_all_counts.m
+++ filter_all_counts.m
@@ -2,11 +2,10 @@ function filter_all_counts(directory)
     filelist = dir(directory);
     
     for(i=1:numel(filelist))
-        %infilename = filelist(i).name
+        infilename = filelist(i).name
         infilename = directory;
-
         if(strcmp(infilename(end-3:end), 'FIN2'))
-            %infilename = strcat(directory, infilename)
+            infilename = strcat(directory, infilename)
             nonblankidx = 143:290;
             [data, k] = read_counts(infilename, nonblankidx);
             not_outliers = outlier_test(data, nonblankidx, 3, 5, k);
