5ed3d364337e3a2ea97a579d343b31d38ec4a74c
Robert Sare <rmsare@stanford.edu>
2017-02-15 17:02:16 -0800

    Comment clarifying non-blank indices input

--- filter_all_counts.m
+++ filter_all_counts.m
@@ -5,7 +5,12 @@ function filter_all_counts(directory)
         infilename = filelist(i).name;
         if(strcmp(infilename(end-3:end), 'FIN2'))
             infilename = strcat(directory, infilename);
-            nonblankidx = 143:290;
+            
+            % eventuall replace with function for finding active readings (not background)
+            %nonblankidx = find_analysis_times(data); 
+            % take indices as user input for now
+            nonblankidx = 143:290; % change this for different analysis times
+            
             [data, k] = read_counts(infilename, nonblankidx);
             not_outliers = outlier_test(data, nonblankidx, 3, 0, k);
             save_counts(infilename, data, not_outliers, nonblankidx);
